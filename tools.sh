a1f7d39dc5565eb32f40250e59edb4cb33ff066b
Arwa Alsayed <157454264+Arwa-Y@users.noreply.github.com>
2024-10-26 18:07:55 +0300

    Add files via upload

--- tools.sh
+++ tools.sh
@@ -4,3 +4,21 @@ sudo apt install openssh-server (y)
 sudo systemctl status ssh
 sudo ufw allow ssh
 
+install an SFTP client:
+sudo adduser sftpuser
+sudo gedit /etc/ssh/sshd_config (you can do nano or gedit)
+#Override default SFTP configuration
+Match User sftpuser
+	ForceCommand internal-sftp
+	PasswordAuthentication yes
+	ChrootDirectory /sftp/%u
+	PermitTunnel no
+	AllowAgentForwarding no
+	AllowTcpForwarding no
+	X11Forwarding no
+sudo mkdir -p /sftp/sftpuser
+sudo chown root:root /sftp
+sudo chmod 755 /sftp
+sudo chown sftpuser:sftpuser /sftp/sftpuser
+sudo systemctl restart ssh
+sftp sftpuser@10.0.2.15
