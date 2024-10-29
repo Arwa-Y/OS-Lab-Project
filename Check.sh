914866353dc6b4f08446206495083b4ebf31df5b
mais <maisshahla03@gmail.com>
2024-10-29 19:53:33 +0300

    Update Check.sh

--- Check.sh
+++ Check.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+exec > perm_change.log 2>&1
+
+files=$(find / -perm 777 ! -xtype l)
 
 echo "Files with the permission 777: "
 
-find -perm 777 -exec chmod 700 {} \;
+echo "$files"
 
-exec > perm_change.log 2> /dev/null
+for file in $files
+do
+chmod 700 "$file"
+done
 
-echo "done"
+
+echo "Check Shell Script Done !"
