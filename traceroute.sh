fedb08992dc44307911490c9ccc3c7c1c41a228c
Aisha Abdul Quadir <101647309+AishaAQ@users.noreply.github.com>
2024-10-30 00:38:07 +0300

    Add traceroute.sh

---
+++ traceroute.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+{
+echo "Routing Table"
+route
+echo
+
+echo "Hostname: $(hostname)"
+echo 
+
+local_server=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}')
+
+echo "Testing local DNS server ($local_server)"
+nslookup google.com $local_server
+
+echo "Tracing route to google.com"
+traceroute google.com
+echo
+
+echo "Pinging google.com"
+ping -c 1 -W 5 google.com
+echo
+
+if traceroute $1 
+then
+	echo "Traceroute to $1 succeeded"
+else
+	echo "Traceroute to $1 failed"	
+	echo "Rebooting Machine"
+	#sudo reboot
+fi
+
+} | tee network.log -a
+
