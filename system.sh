c8526aad5ea43f3f9062edbac32d8e72e3d4e9e5
Aisha Abdul Quadir <101647309+AishaAQ@users.noreply.github.com>
2024-10-30 00:39:07 +0300

    Update system.sh

--- system.sh
+++ system.sh
@@ -1,13 +1,15 @@
+#!/bin/bash
+
 {
 	echo "Disk Space of HOME directory"
 	df -h $HOME
 	echo "Disk Usage of HOME directory"
 	du -h $HOME
-} | tee disk_info.log
+} | tee disk_info.log -a
 
 {
 	echo "Free Memory: $(free | grep Mem | awk '{print $4/$2 * 100}') %"
 	echo "Used Memory: $(free | grep Mem | awk '{print $3/$2 * 100}') %"
 	echo "CPU Model: $(lscpu | grep "Model name" | awk -F: '{print $2}' | xargs)"
 	echo "No. of CPU cores: $(nproc)"
-} | tee mem_cpu_info.log
+} | tee mem_cpu_info.log -a
