8b462c89c53a176d6893420f0011f02eea495fab
Aisha Abdul Quadir <101647309+AishaAQ@users.noreply.github.com>
2024-10-29 16:13:31 +0300

    Sent to log file

--- system.sh
+++ system.sh
@@ -1,4 +1,6 @@
-echo "Disk Space of HOME directory"
-df -h $HOME
-echo "Disk Usage of HOME directory"
- du -h $HOME
+{
+	echo "Disk Space of HOME directory"
+	df -h $HOME
+	echo "Disk Usage of HOME directory"
+	du -h $HOME
+} | tee disk_info.log
