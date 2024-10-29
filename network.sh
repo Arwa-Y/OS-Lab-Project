988a91b0361bca7c33c617ab697f9ecb2a7ed751
Aisha Abdul Quadir <101647309+AishaAQ@users.noreply.github.com>
2024-10-30 00:38:36 +0300

    Update network.sh

--- network.sh
+++ network.sh
@@ -3,18 +3,15 @@
 
 for j in $(seq 1 $#); do
 
-	for i in {1..3}; do	
-
-		if ping -c 1 -W 5 ${!j} > /dev/null
-		then
-			echo $(date "+%Y-%m-%d %T") 
-			echo "Connectivity with ${!j} is ok"
-		else
-			echo "Ping Unsuccessful: ${!j} is not reachable"
-		fi
-		
-		echo 
-	
-	done
-
+	if ping -c 1 -W 5 ${!j} > /dev/null
+	then
+		echo $(date "+%Y-%m-%d %T") 
+		echo "Connectivity with ${!j} is ok"
+	else
+		echo "Ping Unsuccessful: ${!j} is not reachable"
+		echo
+		echo "Running traceroute.sh"
+		./traceroute.sh $#
+	fi
+		 
 done
