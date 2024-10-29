bf92cf163603632749a639777d039ea30c5fbf11
Mais Abushahla <157325009+maisy-daisy@users.noreply.github.com>
2024-10-29 23:18:24 +0300

    Update login.sh

--- login.sh
+++ login.sh
@@ -2,11 +2,17 @@
 
 log1="invalid_attempts.log"
 server="192.168.10.30"
-un=client
-pw=password
+#un=client
+#pw=password
 
-if [[$# -ne 2]] then 
+if [[ $# -ne 2 ]]
+then 
 	echo "Format: $0 username password"
-		
+	exit 1		
 fi
+
+un=$1
+pw=$2
+
+echo "$1, $2"
 echo "login Shell Script Done !"
