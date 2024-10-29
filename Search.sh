a390222bb9d94ee7e9d5b9a15ba1fedb066bcf2c
Arwa Alsayed <157454264+Arwa-Y@users.noreply.github.com>
2024-10-29 10:48:19 +0300

    Add files via upload

--- Search.sh
+++ Search.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
 Big_file="bigfile"
-search_date=$(date+"%Y-%m-%d %H:%M:%S")
+search_date=$(date)
 system_administrator="aa2102872@qu.edu.qa"
 
+
 #find the files in your account that are larger than 1M
-find ~ -type f -size +1M > $results_file
+find ~ -type f -size +1M > "$Big_file"
 
 #count number of files found
-Number_Of_Files=$(wc -1 < $Big_file)
+Number_Of_Files=$(wc -l < "$Big_file")
 
 #Store search date and the numberof files found with 1M in bigfile
-echo "Search Date: $search_date" >> $results_file
-echo "Number of files larger than 1M: $Number_Of_Files" >> $results_file
+echo "Search Date: $search_date" >> "$Big_file"
+echo "Number of files larger than 1M: $Number_Of_Files" >> "$Big_file"
 
 #Email the system administrator a message about the contents of the bigfile
-if [ -s $Big_file ]; then
-	mail -s "Larger than 1M File Report" "$system_administrator" <<EOF
+if [ "$Number_Of_Files" -gt 0 ]; then
+	mail -s "Larger than 1M Files Report" "$system_administrator" <<EOF
 	Dear system administrator,
-	No. of files larger than 1M: $Number_Of_Files 
+	No. of files larger than 1M as of $search_date :
+	$(cat "Number_Of_Files")
 	For more details check bigfile.
 	Best regards,
 	system
-	EOF
+EOF
 fi
 
