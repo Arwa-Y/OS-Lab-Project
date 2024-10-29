bf449209002ddf7465c955bed70b01b74d04ca62
Aisha Abdul Quadir <101647309+AishaAQ@users.noreply.github.com>
2024-10-29 16:14:34 +0300

    mem_cpu

--- system.sh
+++ system.sh
@@ -4,3 +4,10 @@
 	echo "Disk Usage of HOME directory"
 	du -h $HOME
 } | tee disk_info.log
+
+{
+	echo "Free Memory: $(free | grep Mem | awk '{print $4/$2 * 100}') %"
+	echo "Used Memory: $(free | grep Mem | awk '{print $3/$2 * 100}') %"
+	echo "CPU Model: $(lscpu | grep "Model name" | awk -F: '{print $2}' | xargs)"
+	echo "NO. of CPU cores: $(nproc)"
+} | tee mem_cpu_info.log
