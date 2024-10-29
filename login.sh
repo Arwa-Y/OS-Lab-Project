cb2f70e4f8b2877eb28081708d9fb5742794a054
Mais Abushahla <157325009+maisy-daisy@users.noreply.github.com>
2024-10-29 23:02:43 +0300

    Update login.sh

--- login.sh
+++ login.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sudo grep 'Failed password' /var/log/auth.log | awk '{print $1, $2, $3, $9}'
+log1="invalid_attempts.log"
+server="192.168.10.30"
+un=client
+pw=password
 
 
 echo "login Shell Script Done !"
