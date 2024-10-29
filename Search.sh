76471d2d146bb5405c815b52a03b057b15a225e0
Arwa Alsayed <157454264+Arwa-Y@users.noreply.github.com>
2024-10-29 11:52:59 +0300

    Add files via upload

--- Search.sh
+++ Search.sh
@@ -4,26 +4,22 @@ Big_file="bigfile"
 search_date=$(date)
 email="aa2102872@qu.edu.qa"
 
-
-#find the files in your account that are larger than 1M
+# Find the files in your account that are larger than 1M
 find ~ -type f -size +1M > "$Big_file"
 
-#count number of files found
+# Count number of files found
 Number_Of_Files=$(wc -l < "$Big_file")
 
-#Store search date and the numberof files found with 1M in bigfile
+# Store search date and the number of files found with 1M in bigfile
 echo "Search Date: $search_date" >> "$Big_file"
 echo "Number of files larger than 1M: $Number_Of_Files" >> "$Big_file"
 
-#Email the system administrator a message about the contents of the bigfile
+# Email the system administrator a message about the contents of the bigfile
 if [ "$Number_Of_Files" -gt 0 ]; then
-	mail -s "Larger than 1M Files Report" "$email" <<EOF
-	Dear system administrator,
-	No. of files larger than 1M:
-	$(cat "Number_Of_Files")
-	For more details check bigfile.
-	Best regards,
-	system
-EOF
+     echo -e "Subject: Larger than 1M Files Report\n\n Dear system administrator,
+No. of files larger than 1M: $Number_Of_Files
+For more details check bigfile.
+Best regards,
+system" | msmtp "$email"
 fi
 
