3a8464b32273e28a7d88848878c5c16c7f6509a0
Aisha Abdul Quadir <101647309+AishaAQ@users.noreply.github.com>
2024-10-30 21:39:58 +0300

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -14,14 +14,14 @@ sudo passwd client2
 sudo adduser client1
 sudo gedit /etc/ssh/sshd_config #you can do nano or gedit
 #Override default SFTP configuration
-#Match User client1
-#	ForceCommand internal-sftp
-#	PasswordAuthentication yes
-#	ChrootDirectory /sftp/%u
-#	PermitTunnel no
-#	AllowAgentForwarding no
-#	AllowTcpForwarding no
-#	X11Forwarding no
+Match User client1
+	ForceCommand internal-sftp
+	PasswordAuthentication yes
+	ChrootDirectory /sftp/%u
+	PermitTunnel no
+	AllowAgentForwarding no
+	AllowTcpForwarding no
+	X11Forwarding no
 sudo mkdir -p /sftp/client1
 sudo chown root:root /sftp
 sudo chmod 755 /sftp
@@ -34,14 +34,14 @@ sftp client1@[server_ip]
 sudo adduser client2
 sudo gedit /etc/ssh/sshd_config #you can do nano or gedit
 #Override default SFTP configuration
-#Match User client2
-#	ForceCommand internal-sftp
-#	PasswordAuthentication yes
-#	ChrootDirectory /sftp/%u
-#	PermitTunnel no
-#	AllowAgentForwarding no
-#	AllowTcpForwarding no
-#	X11Forwarding no
+Match User client2
+	ForceCommand internal-sftp
+	PasswordAuthentication yes
+	ChrootDirectory /sftp/%u
+	PermitTunnel no
+	AllowAgentForwarding no
+	AllowTcpForwarding no
+	X11Forwarding no
 sudo mkdir -p /sftp/client2
 sudo chown root:root /sftp
 sudo chmod 755 /sftp
