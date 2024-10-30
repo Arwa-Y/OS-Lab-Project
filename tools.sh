a486ca8bcdf6e3f9462d8a12afe054b9fe6ca403
Aisha Abdul Quadir <101647309+AishaAQ@users.noreply.github.com>
2024-10-30 21:38:13 +0300

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -1,21 +1,27 @@
-Validate that SSH is installed and enabled:
+#Validate that SSH is installed and enabled:
 sudo apt update
 sudo apt install openssh-server -y
 sudo systemctl status ssh
 sudo ufw allow ssh
 
-install SFTP client 1:
+#Create local accounts for client1 and client2 in server:
+sudo useradd -m client1
+sudo passwd client1
+sudo useradd -m client2
+sudo passwd client2
+
+#install SFTP client 1:
 sudo adduser client1
 sudo gedit /etc/ssh/sshd_config #you can do nano or gedit
 #Override default SFTP configuration
-Match User client1
-	ForceCommand internal-sftp
-	PasswordAuthentication yes
-	ChrootDirectory /sftp/%u
-	PermitTunnel no
-	AllowAgentForwarding no
-	AllowTcpForwarding no
-	X11Forwarding no
+#Match User client1
+#	ForceCommand internal-sftp
+#	PasswordAuthentication yes
+#	ChrootDirectory /sftp/%u
+#	PermitTunnel no
+#	AllowAgentForwarding no
+#	AllowTcpForwarding no
+#	X11Forwarding no
 sudo mkdir -p /sftp/client1
 sudo chown root:root /sftp
 sudo chmod 755 /sftp
@@ -24,18 +30,18 @@ sudo systemctl restart ssh
 #Replace [server_ip with the actual server ip
 sftp client1@[server_ip]
 
-install SFTP client 2:
+#install SFTP client 2:
 sudo adduser client2
 sudo gedit /etc/ssh/sshd_config #you can do nano or gedit
 #Override default SFTP configuration
-Match User client2
-	ForceCommand internal-sftp
-	PasswordAuthentication yes
-	ChrootDirectory /sftp/%u
-	PermitTunnel no
-	AllowAgentForwarding no
-	AllowTcpForwarding no
-	X11Forwarding no
+#Match User client2
+#	ForceCommand internal-sftp
+#	PasswordAuthentication yes
+#	ChrootDirectory /sftp/%u
+#	PermitTunnel no
+#	AllowAgentForwarding no
+#	AllowTcpForwarding no
+#	X11Forwarding no
 sudo mkdir -p /sftp/client2
 sudo chown root:root /sftp
 sudo chmod 755 /sftp
@@ -44,7 +50,7 @@ sudo systemctl restart ssh
 #Replace [server_ip with the actual server ip
 sftp client2@[server_ip]
 
-msmtp client side:
+#msmtp client side:
 sudo apt update
 sudo apt install msmtp msmtp-mta
 gedit ~/.msmtprc
@@ -73,4 +79,7 @@ account default :outlook
 
 chmod 600 ~/.msmtprc
 
+#Installing net-tools and traceroute
+sudo apt install net-tools
+sudo apt install traceroute
 
