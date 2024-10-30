34dc2cae532366660ee8b25dbc6f9f97ebe2dde8
Arwa Alsayed <157454264+Arwa-Y@users.noreply.github.com>
2024-10-30 16:06:29 +0300

    Update Task4.sh

--- Task4.sh
+++ Task4.sh
@@ -1,7 +1,8 @@
 #Set up SSH key to avoid entering a password for each transfer
 ssh-keygen -t rsa -b -C "Client2Password"
 ssh-copy-id Client2@192.168.10.15
+
 #add corn job on the client to run the script every hour
 crontab -e
-#add the following line to schedual it 
+#add the following line to schedual it at top of every hour
 0 * * * * /home/arwa/Clientinfo.sh
