2ac74e6eae205bbab9a7f9297b238af56aa67111
Arwa Alsayed <157454264+Arwa-Y@users.noreply.github.com>
2024-10-30 15:18:22 +0300

    Add files via upload

--- Clientinfo.sh
+++ Clientinfo.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
 saved_info="process_info.log"
-server_user=""
+server_user="client2"
 server_IP="192.168.10.15"
-server_path=""
+server_path="home/arwa/logs"
 
-collect_info(){
+info(){
 echo "Log Date: $(date)\n" >> saved_info
+
 # Process tree of all currently running processes
 echo "Process tree of all currently running processes:" >> saved_info
 pstree >> saved_info
+
 #List of any dead or zombie processes
-echo "\n List of any dead or zombie processes:" >> saved_info
+echo -e "\n List of any dead or zombie processes:" >> saved_info
 ps aux | awk '$8 ~ /Z/ {print $0}' >> saved_info
+
 #CPU usage related to processes
-echo >> saved_info
->> saved_info
+echo -e "\n CPU usage related to processes:" >> saved_info
+ps -eo pid,ppid,cmd,%cpu --sort=-%cpu | head -n 15 >> saved_info
+
 #Memory usage of running processes
-echo >> saved_info
->> saved_info
+echo -e "\n Memory usage of running processes:" >> saved_info
+ps -eo pid,ppid,cmd,%mem --sort=-%mem | head -n 15 >> saved_info
+
 #The top 5 resource-consuming processes (based on CPU or memory usage)
-echo >> saved_info
->> saved_info
+echo -e "\n The top 5 resource-consuming processes:" >> saved_info
+ps -eo pid,ppid,cmd,%cpu,%mem --sort=-%cpu | head -n 6 >> saved_info
+}
+
+info
+
+scp "$saved_info" "${server_user}@${server_IP}:/${server_path}"
