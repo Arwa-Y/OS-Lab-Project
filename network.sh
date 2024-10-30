4e6325aff67e8ce0260fc938aefbc3f2a05f0526
Aisha Abdul Quadir <101647309+AishaAQ@users.noreply.github.com>
2024-10-30 21:28:06 +0300

    Update network.sh

--- network.sh
+++ network.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
+no_of_targets=$#
+log_file="network.log"
 
-for j in $(seq 1 $#); do
+if [ $no_of_targets -eq 0 ]
+then
+	echo "Please provide at least one IP address when running network.sh"
+	exit
+fi
 
-	if ping -c 1 -W 5 ${!j} > /dev/null
+for j in $(seq 1 $no_of_targets); do
+	{
+	echo "Log Date: $(date "+%Y-%m-%d %T")"
+	echo "Pinging  ${!j}"
+	
+	if ping -c 4 -W 5 ${!j} > /dev/null 2>&1
 	then
-		echo $(date "+%Y-%m-%d %T") 
+		echo "Ping Successful" 
 		echo "Connectivity with ${!j} is ok"
 	else
 		echo "Ping Unsuccessful: ${!j} is not reachable"
 		echo
 		echo "Running traceroute.sh"
-		./traceroute.sh $#
+		echo
+		./traceroute.sh ${!j}
 	fi
+	} | tee $log_file -a
 		 
 done
