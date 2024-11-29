73b51e108e9a84fcc3cc71733c63b4c298cf29ab
Arwa Alsayed <157454264+Arwa-Y@users.noreply.github.com>
2024-11-29 19:46:42 +0300

    Add files via upload

---
+++ Java/Clientinfo.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+saved_info="process_info.log"
+server_user="client2"
+server_IP="192.168.1.68"
+server_path="home/client2"
+
+
+pw=$1
+
+info(){
+echo "Log Date: $(date) \n" >> "$saved_info"
+
+# Process tree of all currently running processes
+echo "Process tree of all currently running processes:" >> "$saved_info"
+pstree >> "$saved_info"
+
+#List of any dead or zombie processes
+echo -e "\n List of any dead or zombie processes:" >> "$saved_info"
+ps aux | awk '$8 ~ /Z/ {print $0}' >> "$saved_info"
+
+#CPU usage related to processes
+echo -e "\n CPU usage related to processes:" >> "$saved_info"
+ps -eo pid,ppid,cmd,%cpu --sort=-%cpu | head -n 15 >> "$saved_info"
+
+#Memory usage of running processes
+echo -e "\n Memory usage of running processes:" >> "$saved_info"
+ps -eo pid,ppid,cmd,%mem --sort=-%mem | head -n 15 >> "$saved_info"
+
+#The top 5 resource-consuming processes (based on CPU or memory usage)
+echo -e "\n The top 5 resource-consuming processes:" >> "$saved_info"
+ps -eo pid,ppid,cmd,%cpu,%mem --sort=-%cpu | head -n 6 >> "$saved_info"
+}
+
+while true; do
+info
+sshpass -p "$pw" scp "$saved_info" "${server_user}@${server_IP}:/${server_path}"
+echo "$saved_info successfully coppied"
+sleep 3600
+done
---
+++ Java/Search.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+Big_file="bigfile"
+search_date=$(date)
+email="aa2102872@qu.edu.qa"
+
+# Find the files in your account that are larger than 1M
+find ~ -type f -size +1M > "$Big_file"
+
+# Count number of files found
+Number_Of_Files=$(wc -l < "$Big_file")
+
+# Store search date and the number of files found with 1M in bigfile
+echo "Search Date: $search_date" >> "$Big_file"
+echo "Number of files larger than 1M: $Number_Of_Files" >> "$Big_file"
+
+echo "Email successfully sent to: $email "
+
+# Email the system administrator a message about the contents of the bigfile
+if [ "$Number_Of_Files" -gt 0 ]; then
+     echo -e "Subject: Larger than 1M Files Report\n\n Dear system administrator,
+Current date: $search_date ,
+No. of files larger than 1M: $Number_Of_Files
+For more details check $Big_file.
+Best regards,
+system" | msmtp "$email"
+fi
+
